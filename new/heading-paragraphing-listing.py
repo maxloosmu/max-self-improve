b813113d310040748fc2af2e51dbc973fc71bb32
(Max) Loo Pin Mok <maxloo@smu.edu.sg>
2022-12-14 03:48:45 +0800

    fixed '6 Attributes for Success and How They Relate to Me.html', heading-paragraphing-listing.py and annotated tagCount.py

---
+++ new/heading-paragraphing-listing.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+import os
+
+path = "/mnt/c/Users/Max/max-self-improve"
+os.chdir(path)
+new_html = ''
+
+def read_html_to_remove_last_blank_lines(file_path):
+  new_html_list = []
+  with open(file_path, 'r') as f:
+    for line in f:
+      new_html_list.append(line)
+    f.close()
+  while new_html_list[-1] == '\n':
+    new_html_list.pop(-1)
+  new_html_list[-1] = new_html_list[-1].strip('\n')
+  with open(file_path, 'w') as f:
+    f.write(''.join(new_html_list))
+    f.close()
+
+def read_html_file(file_path):
+  global new_html
+  start_ul = False
+  with open(file_path, 'r') as f:
+    for line in f:
+      if line[:3] == '## ':
+        new_html = new_html + '<h2>' + line[3:-1] + '</h2>\n'
+      elif line[:4] == '### ':
+        new_html = new_html + '<h3>' + line[4:-1] + '</h3>\n'
+      elif line[:2] == '- ' or line[:2] == '+ ' or line[:2] == '* ':
+        if not start_ul:
+          start_ul = True
+          new_html = new_html + '<ul>\n'
+        new_html = new_html + '<li>' + line[2:-1] + '</li>\n'
+      elif line == '\n':
+        if start_ul:
+          start_ul = False
+          new_html = new_html + '</ul>\n'
+        new_html = new_html + '\n'
+      elif line[0] == '#':
+        new_html = new_html + line[:-1] + '<br/>\n'
+      else:
+        new_html = new_html + '<p>' + line[:-1] + '</p>\n'
+    f.close()
+
+def write_html_file(file_path):
+  global new_html
+  with open(file_path, 'w') as f:
+    f.write(new_html)
+    f.close()
+  new_html = ''
+
+def main():
+  for root, dirs, files in os.walk("."):
+    for name in files:
+      if name.endswith("my Life.html"):
+        file_path = os.path.join(root, name)
+        read_html_to_remove_last_blank_lines(file_path)
+        read_html_file(file_path)
+        write_html_file(file_path)
+
+if __name__ == '__main__':
+  main()
--- new/tagCount.py
+++ new/tagCount.py
@@ -7,13 +7,13 @@ write_path = path + "/tagCount.txt"
 
 os.chdir(path)
 
-mylist = []
-myset = set()
+mylist = []     # list of all occurences of tags
+myset = set()   # set of one occurence of tags
 mydict = {}
 newdict1 = {}
 newdict2 = {}
 
-def read_text_file(file_path):
+def read_html_file(file_path):
   with open(file_path, 'r') as f:
     for line in f:
       first_split = line.split()
@@ -33,14 +33,17 @@ for root, dirs, files in os.walk("."):
     if name.endswith(".html"):
       # file_path = f"{path}/{file}"
       file_path = os.path.join(root, name)
-      read_text_file(file_path)
+      read_html_file(file_path)
 
 for val in myset:
+  # create count dictionary for individual tags
   mydict[val] = mylist.count(val)
+  # sort in alphabetical order based on tags, which are item[0]
   newdict1 = dict(sorted(mydict.items(), key=lambda item: item[0]))
+  # sort based on count, which are item[1], from largest to smallest
   newdict2 = dict(sorted(newdict1.items(), key=lambda item: item[1], reverse=True))
 
-print(newdict2)
+# print(newdict2)
 with open(write_path, 'w') as f:
   f.close()
 with open(write_path, 'a') as f:
